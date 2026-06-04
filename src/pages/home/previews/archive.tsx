b3cd4b754950c7a6de3181a8cee6f90393db2957
bitxeno <bitxeno@duck.com>
2026-06-04 09:27:17 +0800

    feat(preview): image preview add gallery mode
    Signed-off-by: bitxeno <bitxeno@duck.com>

--- src/pages/home/previews/archive.tsx
+++ src/pages/home/previews/archive.tsx
@@ -578,6 +578,7 @@ const Preview = () => {
                     navigate={(name) => {
                       changeFile(name)
                     }}
+                    gallery_props={{ thumbnail: false, preload: 1 }}
                   />
                 </Suspense>
               </Show>
